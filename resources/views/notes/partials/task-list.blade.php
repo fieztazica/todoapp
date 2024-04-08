4cf80c99e50d1a8ec49c4b749ab17b5a48583e34
Dat Hoang <fiezt@outlook.com>
2024-04-08 14:36:31 +0700

    style tasks list in note

--- resources/views/notes/partials/task-list.blade.php
+++ resources/views/notes/partials/task-list.blade.php
@@ -4,12 +4,20 @@
     @foreach ($tasks as $task)
     <li id="task_{{$task->id}}" title="Task #{{$task->id}}">
         <a href="{{route('tasks.edit', ['id' => $task->id, 'task' => $task])}}">
+            @php
+            $green = "bg-green-300 dark:bg-green-900";
+            $none = "bg-gray-200 dark:bg-gray-800";
+            $red = "bg-red-300 dark:bg-red-900";
+            $yellow = "bg-yellow-200 dark:bg-yellow-800";
+            $color;
+
+            if ($task->due_status == 2) { $color=$green; } else if ($task->due_status == 0) { $color=$none;}
+            else
+            if ($task->due_status > 0) { $color=$yellow;} else {$color=$red;} @endphp
             <div class="text-pretty p-2 rounded min-h-48 min-w-48 w-full md:max-w-sm shadow
-                hover:ring-2 transition-all h-full {{$task->done ? " bg-green-300 dark:bg-green-900" : "bg-gray-200
-                dark:bg-gray-800" }}">
+                hover:ring-2 transition-all h-full {{ $color }}">
                 <p class="text-sm italic break-all text-neutral-800 dark:text-neutral-300 truncate text-pretty mt-2">
-                    {{
-                    $task->due_date }}</p>
+                    {{ $task->due_date }} ({{ $task->fromNow }})</p>
                 <div>
                     <h3 class="text-xl font-bold truncate">{{ $task->name }}</h3>
                 </div>
